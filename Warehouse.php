a77c61cd2b867a4f6df34ee8f4e392168aae2893
getechh <121451053+getechh@users.noreply.github.com>
2022-12-26 16:59:24 +0000

    Add files via upload

---
+++ Warehouse.php
@@ -0,0 +1,30 @@
+<?php
+include('head.php');
+include 'data.php'; 
+
+
+?>
+
+
+<main class="w-100">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center mb-2 border-bottom">
+        <h1 class="h2" style="margin-left: 15%">Warehouse Item</h1>
+
+        </div>
+
+
+
+ 		<div class="col-md-12">
+        <div class="card">
+          <div class="card-header bg-success text-white">
+            <b>Warehouse Items </b>
+           <span class="float:right"><a class="btn btn-primary btn-block btn-sm col-sm-2 float-right" href="javascript:void(0)" id="new_supplier">
+          <i class="fa fa-plus"></i> New 
+        </a></span>
+
+
+            <span class="float:right"><a class="btn btn-danger btn-block btn-sm col-sm-2 float-right"  id="view_exam">
+          <i class="fa fa-print"></i> print
+        </a></span>
+            
+          </div>
